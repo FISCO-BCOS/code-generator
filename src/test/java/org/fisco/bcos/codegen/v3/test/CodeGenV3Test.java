7fd02fc6534462570017cd2741103a1ddc1b40fe
Kyon <32325790+kyonRay@users.noreply.github.com>
2023-02-12 16:13:10 +0800

    <fix>(codegen): fix return exception when output type are multi and some is struct list. (#7)

--- src/test/java/org/fisco/bcos/codegen/v3/test/CodeGenV3Test.java
+++ src/test/java/org/fisco/bcos/codegen/v3/test/CodeGenV3Test.java
@@ -72,6 +72,13 @@ public class CodeGenV3Test {
         codeGenTest(ABI_FILE, CONTRACT_NAME);
     }
 
+    @Test
+    public void voteABICodeGen() throws IOException {
+        final String ABI_FILE = "AnonymousVoting.abi";
+        final String CONTRACT_NAME = "AnonymousVoting";
+        codeGenTest(ABI_FILE, CONTRACT_NAME);
+    }
+
     private void codeGenTest(String abiFileName, String contractName) throws IOException {
         String abiFile = CodeGenV3Test.class.getClassLoader().getResource(abiFileName).getPath();
         String javaOutPut = new File(abiFile).getParent() + File.separator + JAVA_OUTPUT_DIR;
